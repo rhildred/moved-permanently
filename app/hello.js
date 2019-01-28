faf89099d489ee278a06844253efd2d05ebaa928
Rich Hildred <rhildred@gmail.com>
2019-01-28 03:07:25 +0000

    made it work on the host network

--- app/hello.js
+++ app/hello.js
@@ -1,12 +1,14 @@
 const http = require('http');
 
+var port = process.env.PORT || parseInt(process.argv.pop()) || 1337;
+
 http.createServer((req, res) => {
   res.writeHead(302, {
     'Location': process.argv[2]
     //add other headers here...
   });
   res.end();
-}).listen(1337, '0.0.0.0', () => {
+}).listen(port, '0.0.0.0', () => {
   console.log('server up redirecting to ' + process.argv[2]);
 
 });
